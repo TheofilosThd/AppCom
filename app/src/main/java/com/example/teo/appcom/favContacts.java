3ce9139a62276eb4e7ab1950a5ebed7acf207cb8
christsop <p3130211@dias.aueb.gr>
2016-11-13 23:10:13 +0200

    Favorite Contacts complete

--- app/src/main/java/com/example/teo/appcom/favContacts.java
+++ app/src/main/java/com/example/teo/appcom/favContacts.java
@@ -1,6 +1,7 @@
 package com.example.teo.appcom;
 
 import android.content.Intent;
+import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -33,68 +34,29 @@ public class favContacts extends AppCompatActivity {
 
         LinearLayout dynamicalContacts = (LinearLayout) findViewById(R.id.spaceForTexts);
 
-        
+
+        ImageButton[] contact = new ImageButton[6];
+        TextView[] contactt = new TextView[6];
+        String[] number = {"11", "22", "33", "44" ,"55", "66"};
+        String[] name = {"Gios", "Gios 2", "Korh", "Eggonh" ,"Antwnis", "Kleidaras"};
+        int[] pic = new int[] { R.drawable.person1, R.drawable.person2, R.drawable.person3, R.drawable.person4, R.drawable.person5, R.drawable.person6 };
 
 
-        final ImageButton contact1 = new ImageButton(this);
-        final TextView contact1t = new TextView(this);
-        contact1t.setText("Γιοκας μου");
-        contact1t.setGravity(Gravity.CENTER);
-        contact1.setImageResource(R.drawable.person1);
-        contact1.setBackgroundColor(0x0000FF10);
-        dynamicalContacts.addView(contact1);
-        dynamicalContacts.addView(contact1t);
-        clickBtn(contact1, "11");
 
-        final ImageButton contact2 = new ImageButton(this);
-        final TextView contact2t = new TextView(this);
-        contact2t.setText("Κορουλα μου");
-        contact2t.setGravity(Gravity.CENTER);
-        contact2.setImageResource(R.drawable.person2);
-        contact2.setBackgroundColor(0x0000FF10);
-        dynamicalContacts.addView(contact2);
-        dynamicalContacts.addView(contact2t);
-        clickBtn(contact2, "22");
+        for (int i = 0; i < 6; i++){
+            contact[i] = new ImageButton(this);
+            contactt[i] = new TextView(this);
+            contactt[i].setText(name[i]);
+            contactt[i].setTextSize(30);
+            contactt[i].setGravity(Gravity.CENTER);
+            contact[i].setImageResource(pic[i]);
+            contact[i].setBackgroundColor(0x0000FF10);
+            dynamicalContacts.addView(contact[i]);
+            dynamicalContacts.addView(contactt[i]);
+            clickBtn(contact[i], number[i]);
+        }
 
-        final ImageButton contact3 = new ImageButton(this);
-        final TextView contact3t = new TextView(this);
-        contact3t.setText("Γαμπρος");
-        contact3t.setGravity(Gravity.CENTER);
-        contact3.setImageResource(R.drawable.person3);
-        contact3.setBackgroundColor(0x0000FF14);
-        dynamicalContacts.addView(contact3);
-        dynamicalContacts.addView(contact3t);
-        clickBtn(contact3, "33");
 
-        final ImageButton contact4 = new ImageButton(this);
-        final TextView contact4t = new TextView(this);
-        contact4t.setText("Εγγονος");
-        contact4t.setGravity(Gravity.CENTER);
-        contact4.setImageResource(R.drawable.person4);
-        contact4.setBackgroundColor(0x0000FF16);
-        dynamicalContacts.addView(contact4);
-        dynamicalContacts.addView(contact4t);
-        clickBtn(contact4, "44");
-
-        final ImageButton contact5 = new ImageButton(this);
-        final TextView contact5t = new TextView(this);
-        contact5t.setText("Εγγονη");
-        contact5t.setGravity(Gravity.CENTER);
-        contact5.setImageResource(R.drawable.person5);
-        contact5.setBackgroundColor(0x0000FF1);
-        dynamicalContacts.addView(contact5);
-        dynamicalContacts.addView(contact5t);
-        clickBtn(contact5, "55");
-
-        final ImageButton contact6 = new ImageButton(this);
-        final TextView contact6t = new TextView(this);
-        contact6t.setText("Γιος 'watch up'");
-        contact6t.setGravity(Gravity.CENTER);
-        contact6.setImageResource(R.drawable.person6);
-        contact6.setBackgroundColor(0x0000FF10);
-        dynamicalContacts.addView(contact6);
-        dynamicalContacts.addView(contact6t);
-        clickBtn(contact6, "66");
     }
 
     public void makeCall(String telephone) {
